d9bc7265a3f9ae7bd9c0ed8e4262b219998c9a34
Sanjay Risbud <sanj19972001@yahoo.com>
2025-05-18 16:33:10 +0800

    added code to generate logs

--- hq_app/main.py
+++ hq_app/main.py
@@ -4,8 +4,10 @@ from util import constants, hive_utils as hu, spark_utils as su
 
 def main() -> None:
     """Application entrypoint."""
+
     with (su.SparkContextManager("hq_app") as scm):
         consolidated_df = scm.read_received_files()
+        scm.logger.info(f"Received {consolidated_df.count()} rows of consolidated data from retailer.")
 
         hu.use_database(scm.spark, constants.HIVE_DATABASE)
         columns_of_customers = hu.get_table_columns(scm.spark, constants.HIVE_TABLE_CUSTOMERS)
@@ -16,6 +18,21 @@ def main() -> None:
                                       columns_of_orders, columns_of_order_items)
         customers_df, orders_df, order_items_df = scm.split_df_into_multiple_dfs(
             flattened_df, columns_of_customers, columns_of_orders, columns_of_order_items)
+        scm.logger.info("Split consolidated data into 3 dataframes.")
+        scm.logger.info(f"Customers = {customers_df.count()} rows.")
+        scm.logger.info(f"Orders = {orders_df.count()} rows.")
+        scm.logger.info(f"Order Items = {order_items_df.count()} rows.")
+
+        hu.insert_df_into_table(customers_df, constants.HIVE_TABLE_CUSTOMERS)
+        hu.insert_df_into_table(orders_df, constants.HIVE_TABLE_ORDERS)
+        hu.insert_df_into_table(order_items_df, constants.HIVE_TABLE_ORDER_ITEMS)
+        scm.logger.info("Processing done!  Final record counts:")
+        scm.logger.info(f"Table {constants.HIVE_TABLE_CUSTOMERS} = "
+                        f"{hu.get_row_count(scm.spark, constants.HIVE_TABLE_CUSTOMERS)} rows.")
+        scm.logger.info(f"Table {constants.HIVE_TABLE_ORDERS} = "
+                        f"{hu.get_row_count(scm.spark, constants.HIVE_TABLE_ORDERS)} rows.")
+        scm.logger.info(f"Table {constants.HIVE_TABLE_ORDER_ITEMS} = "
+                        f"{hu.get_row_count(scm.spark, constants.HIVE_TABLE_ORDER_ITEMS)} rows.")
 
 
 if __name__ == "__main__":  # pragma: no cover
--- retailer_app/main.py
+++ retailer_app/main.py
@@ -6,6 +6,10 @@ def main() -> None:
     """Application entrypoint."""
     with (su.SparkContextManager("retailer_app") as scm):
         customers_df, orders_df, order_items_df = scm.read_files()
+        scm.logger.info("Read table exports into 3 dataframes.")
+        scm.logger.info(f"Customers = {customers_df.count()} rows.")
+        scm.logger.info(f"Orders = {orders_df.count()} rows.")
+        scm.logger.info(f"Order Items = {order_items_df.count()} rows.")
 
         columns_of_customers = customers_df.columns
         columns_of_orders = orders_df.columns
@@ -17,8 +21,8 @@ def main() -> None:
             customer_orders_df, columns_of_customers, columns_of_orders, columns_of_order_items)
         orders_of_same_customer_df = scm.form_orders_of_same_customer_df(
             order_items_of_same_order_df,columns_of_customers, columns_of_orders)
-
         scm.write_df(orders_of_same_customer_df)
+        scm.logger.info(f"To send {orders_of_same_customer_df.count()} rows of consolidated data to HQ.")
 
 
 if __name__ == "__main__":  # pragma: no cover
